d6883529f099e34153b2b2af43712cd619a52db8
Eragon6 <119508559+Eragon6@users.noreply.github.com>
2023-04-12 19:15:39 +0200

    modif thread.c

--- Prozesse&Threads/Ex_5/thread.c
+++ Prozesse&Threads/Ex_5/thread.c
@@ -12,34 +12,45 @@ int sum=0;                          //Die gloabale Variable
 pthread_mutex_t mutex;
 
 void * helloworld(void * arg) {         //Die Funktion, dass jeder thread 1.000.000 mal eine Schleife durchlaufen
-    int ordre = * (int*)arg;
+    int argument = * (int*)arg;
     for (int i = 0; i < 1000000; i++)
     {
         pthread_mutex_lock(&mutex);
-        sum+= ordre;
+        sum+= argument;
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
 int main(int argc, char** argv){    
-    int i, nb, var;                     
+    int i, zh;                     
     int * args;
     pthread_t * threads;
-    nb = atoi(argv[1]);
-    threads = malloc(nb * sizeof(pthread_t));
-    args = malloc(nb * sizeof(int));
+    zh = atoi(argv[1]);
+    threads = malloc(zh * sizeof(pthread_t));
+    args = malloc(zh * sizeof(int));
     pthread_mutex_init(&mutex, NULL);           //initalisierung der mutex
-    for (i = 0; i < nb; i++) {
-        if (i%2==0)                             //die Hälfte addieren in einer Schleife 5 Mal jeweils eine 1 
-            {var=5*1;}
-        else
-            {var=-(5*1);}                       //die Hälfte addieren in einer Schleife 5 Mal jeweils eine 1 
-        
+    for (i = 0; i < zh; i++) {
+        int var=0;
+        if (i%2==0){                             //die Hälfte addieren in einer Schleife 5 Mal jeweils eine 1 
+            for (int j = 0; j < 5; j++)
+            {
+                var+=1;
+            }
+            
+        }
+        else{                                   //die Hälfte subtrahieren in einer Schleife 5 Mal jeweils eine 1 
+            for (int j = 0; j < 5; j++)
+            {
+                var-=1;
+            }
+        }
+
         args[i] = var;
         pthread_create(&threads[i], NULL, helloworld, &args[i]);   //Erzeugung den Threads
     }
-    for (i = 0; i < nb; i++) {
+
+    for (i = 0; i < zh; i++) {
         pthread_join(threads[i], NULL);                 //Sammeleung alle Threads
         printf("thread Nr. %d joined \n", i);
     }
